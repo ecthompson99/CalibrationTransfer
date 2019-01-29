4bc8204a61e1173fd98492aca211d9a9354f016f
Ethan Thompson <ecthompson99@gmail.com>
2019-01-29 11:21:20 -0500

    Added changes from data and troubleshooting to calibrationtransfer

--- CalibrationTransfer.py
+++ CalibrationTransfer.py
@@ -1,27 +1,56 @@
 #Importing modules
-import os
+
 import tkinter as tk
 from tkinter import filedialog
-from openpyxl import load_workbook, Workbook
+from openpyxl import load_workbook
+from matplotlib import pyplot as plt
+from scipy.interpolate import interp1d
+import numpy as np
+from numpy import square as sq
+from scipy.interpolate import InterpolatedUnivariateSpline as spl
+from peakutils import indexes as peak
 
 
 root = tk.Tk()
-root.withdraw()
-wb = Workbook()
+file_path = filedialog.askopenfilenames(filetype = [("Excel File","*.xlsx")])
 
-#Grabbing the file pathes=[("E
-file_path = filedialog.askopenfilenames(filetype = (["Excel File","*.xlsx"]))
-
-#Importing workbook
 first = load_workbook(str(file_path[0]))
 second = load_workbook(str(file_path[1]))
 
-sheet = first.get_sheet_by_name('ABS')
+sheet1 = first['ABS']
+sheet2 = second['ABS']
 
-spegg1 = list(range(sheet.max_columns)
+# Fills up a list with an entire row
+def fill_list(sheet, r, start = 1, length=sheet1.max_column):
+    """ Fills up a list with an entire row from the excel sheet.
+    
+        Take two mandatory arguments, the sheetname and the row you want to scrape.
+        
+    """
+    vals = []
+    for i in range(start, length+1): 
+        vals.append(sheet.cell(r, i).value)
+    return vals
 
-for i in range(1,sheet.max_rows+1):
-    for j in range(1, sheet.max_columns+1):
-    spegg1[i].append(sheet(i, j).value)
+# Saving a 1D array of the x-values of each instrument
+first_x = fill_list(sheet1, 1, 7)
+first_x2 = fill_list(sheet2, 1, 7)
 
-print(spegg1)
+row = int(input('which row do you want to scrape?: '))
+
+# saving a 1D array of the first set of y-values of each instrument (should correspond to the same sample)
+first_row = fill_list(sheet1, row, 7)
+first_row2 = fill_list(sheet2, row, 7)
+
+interpolation_min = int(input('what is the lower limit of interpolation?: '))
+interpolation_max = int(input('what is the upper limit of interpolation?: '))
+
+x = np.linspace(interpolation_min,interpolation_max,251)
+
+#interpolation: we can know all y-values between 450 and 700 nm. 
+y1 = spl(first_x, first_row) # function
+y2 = spl(first_x2, first_row2) # function
+
+# interpolated y-values
+yy1 = y1(x)
+yy2 = y2(x)
